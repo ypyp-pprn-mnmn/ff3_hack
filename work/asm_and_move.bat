02ce1207396f32bb8dbd4095029c4b35d8fc251c
take-the-bath <956539+ypyp-pprn-mnmn@users.noreply.github.com>
2017-11-03 18:48:25 +0900

    after stumled around git...

---
+++ work/asm_and_move.bat
@@ -0,0 +1,2 @@
+	nesasm %1.asm
+	move %1.nes "release\%1.nes"
---
+++ work/ff3_make_versions.bat
@@ -0,0 +1,12 @@
+@echo off
+	rem nesasm ff3_hack.asm
+	nesasm ff3_hack_beta.asm
+	nesasm ff3_hack_another.asm
+	nesasm ff3_hack_noitem99.asm
+	nesasm ff3_hack_soundtest.asm
+	rem move ff3_hack.nes "release\ff3_hack.nes"
+	move ff3_hack_beta.nes "release\ff3_hack_beta.nes"
+	move ff3_hack_another.nes "release\ff3_hack_another.nes"
+	move ff3_hack_noitem99.nes "release\ff3_hack_noitem99.nes"
+	move ff3_hack_soundtest.nes "release\ff3_soundtest.nes"
+@echo on
