6467806c4e4d2e629bf6ff86cff6ef1e75f1efe2
take-the-bath <956539+ypyp-pprn-mnmn@users.noreply.github.com>
2017-11-03 20:24:24 +0900

    restructured directory tree & fixed wired data bug making some encounter hang

--- work/ff3_make_versions.bat
+++ work/ff3_make_versions.bat
@@ -4,6 +4,7 @@
 	nesasm ff3_hack_another.asm
 	nesasm ff3_hack_noitem99.asm
 	nesasm ff3_hack_soundtest.asm
+	nesasm ff3_hack_extra_map.asm
 	rem move ff3_hack.nes "release\ff3_hack.nes"
 	move ff3_hack_beta.nes "release\ff3_hack_beta.nes"
 	move ff3_hack_another.nes "release\ff3_hack_another.nes"
