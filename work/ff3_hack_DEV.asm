0b33904386444dbf6cd8f287a238cb611258f3e7
take-the-bath <956539+ypyp-pprn-mnmn@users.noreply.github.com>
2017-11-04 17:12:44 +0900

    added 'DEV' version

---
+++ work/ff3_hack_DEV.asm
@@ -0,0 +1,16 @@
+; ff3_hack_extra_map.asm
+;
+; description:
+;	creates hacked file with features under development (and sometimes unstable) included
+;
+;======================================================================================================
+;assemble flags
+BALANCED_VERSION
+EXTRA_MAP
+
+EXPERIMENTAL
+EXPERIMENT_IMPL = 5	; encount test
+
+FAST_FIELD_WINDOW
+; the below should be final line of this file
+	.include "ff3_hack_beta.asm"
