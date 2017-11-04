aa2abb7484fbb5efbaa4303250c4ed96914d9600
take-the-bath <pinkishrison@hotmail.com>
2017-11-05 03:54:52 +0900

    fixed sprite priority issue

--- work/ff3_asm.h
+++ work/ff3_asm.h
@@ -13,6 +13,7 @@ field_callSoundDriver		= $c750
 field_updateTileAttrCache	= $c98f
 field_setTileAttrForWindow	= $c9a9
 field_update_vram_by_07d0	= $cb6b	;[in] $07d0[16]: vram address low, $07e0[16]: vram high, $07f0[16]: vram value
+field_hide_sprites_around_window = $ec18
 field_restore_bank			= $ecf5	;[in] $57: bank
 ;field_fill_07c0_ff			= $ed56
 field_loadWindowTileAttr	= $ed56
