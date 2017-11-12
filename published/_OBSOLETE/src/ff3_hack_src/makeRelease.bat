35a26aa4d77d312559545548370bf2a600baf908
take-the-bath <pinkishrison@hotmail.com>
2017-11-13 07:02:01 +0900

    restrucutreed directory tree

--- published/_OBSOLETE/src/ff3_hack_src/makeRelease.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-svn export --force release "../released files/pack"
-cd "../released files/pack"
-"F:\Program Files\7-Zip\7z" a -tzip "ff3_hack.zip" "*" -x!*.nes -x!.svn -x!*.zip -x!todo.txt
-cd "../../work"
-@echo on
