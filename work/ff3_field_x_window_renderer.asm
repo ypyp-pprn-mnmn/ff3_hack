d2c66b84ac24f42c0e9992070eeeb5de5bb3db97
ypyp-pprn-mnmn <956539+ypyp-pprn-mnmn@users.noreply.github.com>
2017-12-04 00:39:31 +0900

    window impl: poc impl, not good perf

--- work/ff3_field_x_window_renderer.asm
+++ work/ff3_field_x_window_renderer.asm
@@ -140,61 +140,104 @@ field_x.store_PPUDATA = $f7b0
 	sta <.widths
 	beq .calc_heights
 		sty .second_corner_parts
+		sty .second_corner_parts+2
+;; heights ignore top/bottom border.
 .calc_heights:
 	;; --- calc heights
-	lda <.height
+	;lda <.height
+	ldx <.height
+	dex
+	dex
+	txa
 	pha	;height for upper
 	clc
 	adc <.top
-	cmp #30
+	;cmp #30
+	cmp #29
 	pla
 	bcc .no_wrap_y
 		;;carry is always set
-		lda #30
+		;lda #30
+		lda #29
 		sbc <.top
 .no_wrap_y:
 	sta <.heights+1	;height in 1st bg
 	eor #$ff
 	sec
 	adc <.height
+	sec
+	sbc #2	;border excl
 	sta <.heights	;height in 2nd bg
 ;	beq .init_offsets
 ;		sty .second_corner_parts+4
+;		sty .second_corner_parts+6
 ;---
-.init_offsets:
-	lda <.left
-	sta <.offset_x
-	lda <.top
-	sta <.offset_y
 
 	jsr field_x.begin_ppu_update
-	
-	jsr .do_render
-	;jsr .do_render
-
-	;jsr .do_render
-	;jsr .do_render
-
+		jsr .render_top_and_bottom
+		lda #%00000100
+		jsr field_x.switch_vram_addr_mode
+		jsr .render_left_and_right
+		lda #%00000000
+		jsr field_x.switch_vram_addr_mode
 	jmp field_x.end_ppu_update
 
-.do_render:
-	ldx #1
-.loop:
+.render_top_and_bottom:
+	lda <.top
+	sta <.offset_y
+	jsr .render_horizontal
+	ldx <.height
+	dex
 	txa
-	pha
+	jsr field_x.calc_y_offset
+.render_horizontal:
+	lda <.left
+	sta <.offset_x
+	lda <.widths+1
+	jsr .do_render
+	jsr field_x.calc_x_offset
+	lda <.widths
+	jmp .do_render
+
+.render_left_and_right:
+	lda <.left
+	sta <.offset_x
+	jsr .render_vertical
+	ldx <.width
+	dex
+	txa
+	jsr field_x.calc_x_offset
+.render_vertical:
+	lda <.top
+	sta <.offset_y
+	inc <.offset_y	;we have skipped top border in heights calc
+	lda <.heights+1
+	jsr .do_render
+	clc
+	adc #1	;we have skipped top border in heights calc
+	jsr field_x.calc_y_offset
+	lda <.heights
+	jmp .do_render
+
+.do_render:
+	;ldx #1
+.loop:
+	;txa
+	;pha
 ;---
-	lda <.widths,x
+	;lda <.widths,x
+	pha
 	beq .skip_draw
 	;---
 		pha
 		;; $3a = x
 		;; $3b = y
 		jsr field.setVramAddrForWindow
-		pla	;width
-		pha
-		clc
-		adc <.offset_x
-		sta <.offset_x
+		;pla	;width
+		;pha
+		;clc
+		;adc <.offset_x
+		;sta <.offset_x
 		;;
 		pla
 		eor #$ff
@@ -229,17 +272,52 @@ field_x.store_PPUDATA = $f7b0
 	.render_end:
 .skip_draw:
 	inc <.render_sequence
-	pla
-	tax
-	dex
-	bpl .loop
+	;pla
+	;tax
+	;dex
+	;bpl .loop
+	pla	;size (width or height)
 	rts
 .default_first_corners:
+	;.db $f7,$00,$fc,$00,$f7,$00,$f9,$00
 	.db $f7,$00,$fc,$00,$00,$00,$00,$00
 .default_second_corners:
+	;.db $f9,$f9,$fe,$fe,$fc,$fc,$fe,$fe
 	.db $f9,$f9,$fe,$fe,$00,$00,$00,$00
 .middle_parts:
 	.db $f8,$f8,$fd,$fd,$fa,$fa,$fb,$fb
+
+field_x.calc_x_offset:
+.left = $38
+.offset_x = $3a
+	clc
+	adc <.left
+	sta <.offset_x
+	rts
+
+field_x.calc_y_offset:
+.top = $39
+.offset_y = $3b
+	clc
+	adc <.top
+	cmp #30
+	bcc .no_wrap_y
+		sbc #30
+.no_wrap_y:
+	sta <.offset_y
+	rts
+
+field_x.switch_vram_addr_mode:
+	pha
+	lda <field.ppu_ctrl_cache
+	and #%11111011
+	sta <field.ppu_ctrl_cache
+	pla
+	and #%00000100
+	ora <field.ppu_ctrl_cache
+	sta <field.ppu_ctrl_cache
+	sta $2000
+	rts
 
 	.if 0
 ;in: A = offset Y, X = offset X
